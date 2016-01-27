6165d95b643ca0ea3dc79aac78f3450753e9f9e0
rekirt <rekirt@163.com>
2016-01-28 00:04:29 +0800

    提交新功能

--- app/src/main/java/com/example/nodeprogress/MainActivity.java
+++ app/src/main/java/com/example/nodeprogress/MainActivity.java
@@ -1,16 +1,24 @@
 package com.example.nodeprogress;
 
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.graphics.drawable.DrawableUtils;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    NodeProgress mp_4;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mp_4 = (NodeProgress)findViewById(R.id.mp_4);
+//        Drawable drawable = getResources().getDrawable(R.drawable.circle_red, getTheme());
+        Drawable drawable = getResources().getDrawable(R.drawable.circle_red);
+        mp_4.setProgressingDrawable(drawable);
     }
 
 }
--- app/src/main/java/com/example/nodeprogress/NodeProgress.java
+++ app/src/main/java/com/example/nodeprogress/NodeProgress.java
@@ -25,11 +25,10 @@ public class NodeProgress extends View{
 	private Drawable progresSuccDrawable;
 	private int nodeRadius;
 	private int processingLineColor;
-
+	private float textSize;
 //	private int progressLineHeight;
 	private int currNodeNO;
 	private int currNodeState;
-//	private int textSize;
 	Context mContext;
 
 	int mWidth,mHeight;
@@ -52,6 +51,7 @@ public class NodeProgress extends View{
 		TypedArray mTypedArray = context.obtainStyledAttributes(attrs,R.styleable.MutiProgress);
 		nodesNum = mTypedArray.getInteger(R.styleable.MutiProgress_nodesNum, 1);
 		nodeRadius = mTypedArray.getDimensionPixelSize(R.styleable.MutiProgress_nodeRadius, 10);
+		textSize = mTypedArray.getDimension(R.styleable.MutiProgress_textSize,20);
 		progressingDrawable = mTypedArray.getDrawable(R.styleable.MutiProgress_progressingDrawable);
 		unprogressingDrawable = mTypedArray.getDrawable(R.styleable.MutiProgress_unprogressingDrawable);
 		progresFailDrawable = mTypedArray.getDrawable(R.styleable.MutiProgress_progresFailDrawable);
@@ -61,6 +61,40 @@ public class NodeProgress extends View{
 		currNodeNO = mTypedArray.getInt(R.styleable.MutiProgress_currNodeNO, 1);
 
 	}
+
+
+	public Drawable getUnprogressingDrawable() {
+		return unprogressingDrawable;
+	}
+
+	public void setUnprogressingDrawable(Drawable unprogressingDrawable) {
+		this.unprogressingDrawable = unprogressingDrawable;
+	}
+
+	public Drawable getProgressingDrawable() {
+		return progressingDrawable;
+	}
+
+	public void setProgressingDrawable(Drawable progressingDrawable) {
+		this.progressingDrawable = progressingDrawable;
+	}
+
+	public Drawable getProgresFailDrawable() {
+		return progresFailDrawable;
+	}
+
+	public void setProgresFailDrawable(Drawable progresFailDrawable) {
+		this.progresFailDrawable = progresFailDrawable;
+	}
+
+	public Drawable getProgresSuccDrawable() {
+		return progresSuccDrawable;
+	}
+
+	public void setProgresSuccDrawable(Drawable progresSuccDrawable) {
+		this.progresSuccDrawable = progresSuccDrawable;
+	}
+
 	@Override
 	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
 		super.onMeasure(widthMeasureSpec, heightMeasureSpec);
@@ -74,18 +108,19 @@ public class NodeProgress extends View{
 		mPaint.setStrokeJoin(Paint.Join.ROUND);
 		mPaint.setStrokeCap(Paint.Cap.ROUND);
 		mCanvas = new Canvas(mBitmap);
-		
 		nodes = new ArrayList<Node>();
-		float nodeWidth = ((float)mWidth)/(nodesNum-1);
+		float nodeWidth = ((float)mWidth)/(nodesNum+1);
 		for(int i=0;i<nodesNum;i++)
 		{
 			Node node = new Node();
+			node.level = "V"+(i+10);
+			node.score = (i+1)*100;
 			if(i==0)
-				node.mPoint = new Point(((int)nodeWidth*i),mHeight/2-nodeRadius);
+				node.mPoint = new Point(((int)nodeWidth*(i+1))-nodeRadius,mHeight/2-nodeRadius);
 			else if(i==(nodesNum-1))
-				node.mPoint = new Point(((int)nodeWidth*i)-nodeRadius*2,mHeight/2-nodeRadius);
+				node.mPoint = new Point(((int)nodeWidth*(i+1))-nodeRadius,mHeight/2-nodeRadius);
 			else
-				node.mPoint = new Point(((int)nodeWidth*i)-nodeRadius,mHeight/2-nodeRadius);
+				node.mPoint = new Point(((int)nodeWidth*(i+1))-nodeRadius,mHeight/2-nodeRadius);
 			if(currNodeNO == i)
 				node.type = 1;
 			else
@@ -93,6 +128,8 @@ public class NodeProgress extends View{
 			nodes.add(node);
 		}
 	}
+
+
 	@Override
 	protected void onDraw(Canvas canvas) {
 		super.onDraw(canvas);
@@ -106,38 +143,63 @@ public class NodeProgress extends View{
 		{
 			Node node = nodes.get(i);
 			Log.v("ondraw", node.mPoint.x +";y="+node.mPoint.y);
+			mPaint.setTextSize(textSize);
 			if(i<currNodeNO)
 			{
-				progressingDrawable.setBounds(node.mPoint.x,  node.mPoint.y , node.mPoint.x + nodeRadius*2,node.mPoint.y + nodeRadius*2);
+				progressingDrawable.setBounds(node.mPoint.x, node.mPoint.y, node.mPoint.x + nodeRadius * 2,node.mPoint.y + nodeRadius*2);
+				Rect rect = progressingDrawable.getBounds();
 				progressingDrawable.draw(canvas);
+				float textWidth = mPaint.measureText(node.level);
+				float textHeight = mPaint.measureText("V");
+				float scoreWidth = mPaint.measureText(node.score+"");
+				canvas.drawText(node.level, rect.centerX() - textWidth / 2, rect.centerY() + textHeight / 2, mPaint);
+				canvas.drawText(node.score + "", rect.centerX() - scoreWidth / 2, rect.centerY() + rect.width(), mPaint);
 			}
 			else if(i==currNodeNO)
 			{
 				if(currNodeState == 1)
 				{
 					progresSuccDrawable.setBounds(node.mPoint.x,  node.mPoint.y , node.mPoint.x + nodeRadius*2,node.mPoint.y + nodeRadius*2);
+					Rect rect = progresSuccDrawable.getBounds();
 					progresSuccDrawable.draw(canvas);
+					float textWidth = mPaint.measureText(node.level);
+					float textHeight = mPaint.measureText("V");
+					float scoreWidth = mPaint.measureText(node.score + "");
+					canvas.drawText(node.level, rect.centerX() - textWidth / 2, rect.centerY() + textHeight / 2, mPaint);
+					canvas.drawText(node.score + "", rect.centerX() - scoreWidth / 2, rect.centerY() + rect.width(), mPaint);
+
 				}
 				else
 				{
-					progresFailDrawable.setBounds(node.mPoint.x,  node.mPoint.y , node.mPoint.x + nodeRadius*2,node.mPoint.y + nodeRadius*2);
+					progresFailDrawable.setBounds(node.mPoint.x, node.mPoint.y, node.mPoint.x + nodeRadius * 2, node.mPoint.y + nodeRadius * 2);
+					Rect rect = progresFailDrawable.getBounds();
 					progresFailDrawable.draw(canvas);
+					float textWidth = mPaint.measureText(node.level);
+					float textHeight = mPaint.measureText("V");
+					float scoreWidth = mPaint.measureText(node.score + "");
+					canvas.drawText(node.level, rect.centerX() - textWidth / 2, rect.centerY() + textHeight / 2, mPaint);
+					canvas.drawText(node.score + "", rect.centerX() - scoreWidth / 2, rect.centerY() + rect.width(), mPaint);
 				}
 			}
 			else
 			{
-				unprogressingDrawable.setBounds(node.mPoint.x,  node.mPoint.y , node.mPoint.x + nodeRadius*2,node.mPoint.y + nodeRadius*2);
+				unprogressingDrawable.setBounds(node.mPoint.x, node.mPoint.y, node.mPoint.x + nodeRadius * 2, node.mPoint.y + nodeRadius * 2);
+				Rect rect = unprogressingDrawable.getBounds();
 				unprogressingDrawable.draw(canvas);
+				float textWidth = mPaint.measureText(node.level);
+				float textHeight = mPaint.measureText("V");
+				float scoreWidth = mPaint.measureText(node.score+"");
+				canvas.drawText(node.level,rect.centerX()-textWidth/2,rect.centerY()+textHeight/2,mPaint);
+				canvas.drawText(node.score+"",rect.centerX()-scoreWidth/2,rect.centerY()+rect.width(),mPaint);
 			}
 		}
 	}
 	private void DrawProgerss()
 	{
 		Paint bgPaint = new Paint();
-		bgPaint.setColor(Color.parseColor("#f0f0f0"));
+		bgPaint.setColor(Color.parseColor("#FFFFFF"));
 		mCanvas.drawRect(0, 0, mWidth, mHeight, bgPaint);
 		mPaint.setStrokeWidth(nodeRadius/2);
-//		mCanvas.drawLine(nodeRadius, mHeight/2, mWidth-nodeRadius, mHeight/2, mPaint);  //�߶�2��ȥ��nodeRadius
 		mCanvas.drawLine(nodeRadius, mHeight/2, nodes.get(currNodeNO).mPoint.x + nodeRadius, nodes.get(currNodeNO).mPoint.y + nodeRadius, mPaint);  //�߶�2��ȥ��nodeRadius
 		mPaint.setColor(Color.parseColor("#dddddd"));
 		mCanvas.drawLine(nodes.get(currNodeNO).mPoint.x +nodeRadius, nodes.get(currNodeNO).mPoint.y + nodeRadius, mWidth-nodeRadius, mHeight/2, mPaint);  //�߶�2��ȥ��nodeRadius
@@ -146,5 +208,7 @@ public class NodeProgress extends View{
 	{
 		Point mPoint;
 		int type;
+		String level;
+		int score;
 	}
 }
